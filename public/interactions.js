9caa0bae04a063c6146b4e7a5ade95a6df64e461
Kimberly Suazo  & Soren Zeliger <pair+kimberlysuazo.soreasy@devbootcamp.com>
2016-11-12 20:02:45 -0500

    demos are starting

--- public/interactions.js
+++ public/interactions.js
@@ -1,45 +1,46 @@
-$( document ).ready(function() {
-    var Correct = 0
-    var GameStarted = false
-    var Incorrect = 0
-    var Turn = 0
-    var CurrentNotes = []
-
-    // when top bar of notes are clicked
-    $('body').on('click', '.notes', function(event) {
-        console.log("sup sup")
-        let note = $(event.target).text().trim()
-        if (Game) {
-            console.log("game is live")
-        }
-        // play corresponding sound
-    })
-
-    // when Start Game is clicked
-    $('#start-game').on('click', function () {
-        $(this).remove()
-        $('body').append('<p>Correct: '+Correct+'</p>')
-        $('body').append('<p>Incorrect: '+Incorrect+'</p>')
-        GameStarted = true
-        displaySelectedNotes()
-        playNextThree()
-    })
-
-    function displaySelectedNotes() {
-        console.log(Turn)
-        CurrentNotes = []
-        $('body').append('<p>Your Current Notes Are: </p>')
-        $('body').append($('.notes').splice(Turn*3,Turn*3+3))
-        CurrentNotes.concat($('.notes').splice(Turn*3,Turn*3+3))
-        // debugger;
-    }
-
-    function playNextThree() {
-        let thirdNote = Turn*3 + 3;
-        for (let i = Turn*3; i < thirdNote; i++) {
-            // play corresponding sound
-        }
-        playRandomNote()
-    }
-
-});
+// $( document ).ready(function() {
+//     var Correct = 0
+//     var GameStarted = false
+//     var Incorrect = 0
+//     var Turn = 0
+//     var CurrentNotes = []
+//
+//     // when top bar of notes are clicked
+//     $('body').on('click', '.notes', function(event) {
+//         console.log("sup sup")
+//         let note = $(event.target).text().trim()
+//         if (GameStarted) {
+//             console.log("game is live")
+//         }
+//         debugger;
+//         // play corresponding sound
+//     })
+//
+//     // when Start Game is clicked
+//     $('#start-game').on('click', function () {
+//         $(this).remove()
+//         $('body').append('<p>Correct: '+Correct+'</p>')
+//         $('body').append('<p>Incorrect: '+Incorrect+'</p>')
+//         GameStarted = true
+//         displaySelectedNotes()
+//         playNextThree()
+//     })
+//
+//     function displaySelectedNotes() {
+//         console.log(Turn)
+//         CurrentNotes = []
+//         $('body').append('<p>Your Current Notes Are: </p>')
+//         $('body').append($('.notes').slice(Turn*3,Turn*3+3).clone())
+//         $('body').append(CurrentNotes)
+//         // debugger;
+//     }
+//
+//     function playNextThree() {
+//         let thirdNote = Turn*3 + 3;
+//         for (let i = Turn*3; i < thirdNote; i++) {
+//             // play corresponding sound
+//         }
+//         playRandomNote()
+//     }
+//
+// });
--- public/sketch.js
+++ public/sketch.js
@@ -4,11 +4,14 @@
 //
 // test();
 //
-var notes = [60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71];
+
+var notes = [69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80];
 
 // For automatically playing the song
 var index = 0;
 var playback = [];
+var justPlayedSound = false;
+var playedNote = "blah";
 
 // function setSong() {
 //   playback.push({ note: 10, duration: 100, display: "A" });
@@ -52,12 +55,73 @@ function playNote(note, duration) {
 }
 
 // When we click
-function mousePressed() {
-  // Map mouse to the key index
-  playNote(notes[4]);
-}
+// function mousePressed() {
+//   // Map mouse to the key index
+//   playNote(59, 500);
+// }
 
 // Fade it out when we release
 function mouseReleased() {
   osc.fade(0,0.5);
 }
+
+$( document ).ready(function() {
+    var Correct = 0
+    var GameStarted = false
+    var Incorrect = 0
+    var Turn = 0
+    var CurrentNotes = []
+
+
+    // when top bar of notes are clicked
+    $('body').on('click', '.notes', function(event) {
+        console.log("sup sup")
+        let note = $(event.target).text().trim()
+        if (GameStarted) {
+            console.log("game is live")
+        }
+        // debugger;
+        if (justPlayedSound) {
+
+        }
+        playNote(70, 500)
+    })
+
+    // when Start Game is clicked
+    $('#start-game').on('click', function () {
+        $(this).remove()
+        $('body').append('<p>Correct: '+Correct+'</p>')
+        $('body').append('<p>Incorrect: '+Incorrect+'</p>')
+        GameStarted = true
+        // displaySelectedNotes()
+        // playNextThree()
+        playRandomNote()
+    })
+
+    function playRandomNote() {
+        let playedNoteLocal = [69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80][Math.floor(Math.random()*12)]
+        playedNote = playedNoteLocal
+        playNote(playedNoteLocal, 600)
+        justPlayedSound = true
+    }
+
+    function displaySelectedNotes() {
+        console.log(Turn)
+        CurrentNotes = []
+        $('body').append('<p>Your Current Notes Are: </p>')
+        $('body').append($('.notes').slice(Turn*3,Turn*3+3).clone())
+        $('body').append(CurrentNotes)
+        // debugger;
+    }
+
+    function playNextThree() {
+        // let thirdNote = Turn*3 + 3;
+        let first = Turn*3
+        let deezNotes = [69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80];
+        playNote(deezNotes[first], 600)
+        playNote(deezNotes[first+1], 600)
+        playNote(deezNotes[first+2], 600)
+
+    }
+
+});
