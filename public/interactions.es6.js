371042a76c82017a09559f6f313456e235805c76
Jenn Su & Kimberly Suazo  & Soren Zeliger <pair+jennysoup.kimberlysuazo.soreasy@devbootcamp.com>
2016-11-15 12:06:05 -0500

    change sound library from p5 to band.js and define playSound function

--- public/interactions.es6.js
+++ public/interactions.es6.js
@@ -1,8 +1,24 @@
+function playSound(noteAry) {
+
+    var conductor = new BandJS()
+    conductor.setTimeSignature(4,4)
+    conductor.setTempo(120)
+    var piano = conductor.createInstrument()
+
+    noteAry.forEach((note) => {
+        piano.note('whole', note)
+    })
+
+    var player = conductor.finish()
+    player.play()
+}
+
 $( document ).ready(function() {
     var Correct = 0
     var GameStarted = false
     var Incorrect = 0
     var Turn = 0
+    var notes = ['A4', 'A#4', 'B4', 'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4']
 
     // when top bar of notes are clicked
     $('body').on('click', '.notes', function(event) {
@@ -27,15 +43,17 @@ $( document ).ready(function() {
     function displaySelectedNotes() {
         console.log(Turn)
         $('body').append('<p>Your Current Notes Are: </p>')
-        $('body').append($('.notes').splice(Turn*3,Turn*3+3))
+        $('body').append($('.notes').clone().splice(Turn*3,Turn*3+3))
         // debugger;
     }
 
     function playNextThree() {
         let thirdNote = Turn*3 + 3;
+        var notesToPlay = []
         for (let i = Turn*3; i < thirdNote; i++) {
-            // play corresponding sound
+            notesToPlay.push(notes[i])
         }
+        playSound(notesToPlay)
     }
 
 });
