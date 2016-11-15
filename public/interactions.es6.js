c01b08977b80bced04a45b32ef98ead0b3aaff4c
Jenn Su & Kimberly Suazo  & Soren Zeliger <pair+jennysoup.kimberlysuazo.soreasy@devbootcamp.com>
2016-11-15 12:46:40 -0500

    Setup listener for user guessed note

--- public/interactions.es6.js
+++ public/interactions.es6.js
@@ -19,6 +19,7 @@ $( document ).ready(function() {
     var Incorrect = 0
     var Turn = 0
     var notes = ['A4', 'A#4', 'B4', 'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4']
+    var guessedNote
 
     // when top bar of notes are clicked
     $('body').on('click', '.notes', function(event) {
@@ -37,23 +38,40 @@ $( document ).ready(function() {
         $('body').append('<p>Incorrect: '+Incorrect+'</p>')
         GameStarted = true
         displaySelectedNotes()
-        playNextThree()
+        playGame()
+    })
+
+    $('body').on('click', '.current', function(event) {
+        guessedNote = $(event.target).parent().attr('id')
+        debugger;
+        // marks this note as the guessed one
     })
 
     function displaySelectedNotes() {
         console.log(Turn)
         $('body').append('<p>Your Current Notes Are: </p>')
-        $('body').append($('.notes').clone().splice(Turn*3,Turn*3+3))
+        let currNotes = $('.notes').clone().splice(Turn*3,Turn*3+3)
+        $(currNotes).addClass('current')
+        $('body').append(currNotes)
         // debugger;
     }
 
-    function playNextThree() {
+    function playGame() {
         let thirdNote = Turn*3 + 3;
         var notesToPlay = []
         for (let i = Turn*3; i < thirdNote; i++) {
             notesToPlay.push(notes[i])
         }
         playSound(notesToPlay)
+
+        // until the game is over
+            // play sound and then check user guess
+    }
+
+    function playRandomNote(notes) {
+        let selectedSound = notes[Math.floor(Math.random()*notes.length)]
+        playSound([selectedSound])
+
     }
 
 });
